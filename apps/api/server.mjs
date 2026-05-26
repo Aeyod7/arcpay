f4660f7288aaf48598ca6d984d90fb7bed5c0ba9
Your Name <you@example.com>
2026-05-26 18:31:43 +0100

    feat: use relative custom generated prisma client for bulletproof serverless bundling

--- apps/api/server.mjs
+++ apps/api/server.mjs
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './prisma/client/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
